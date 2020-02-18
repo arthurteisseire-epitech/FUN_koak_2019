229d4bb173ca42835d3e9c7301df103d969e03cc
arthur <arthur.teisseire@epitech.eu>
2020-02-18 12:14:27 +0100

    Add koakToLLVM for sub float

--- src/KoakToLLVM.hs
+++ src/KoakToLLVM.hs
@@ -5,26 +5,33 @@ import           KoakAST
 import           LLVM.AST                   as AST
 import           LLVM.AST.CallingConvention as AST
 import           LLVM.AST.Constant          as C
+import           LLVM.AST.Float             as F
 
+import Data.Maybe
 
-koakToLLVM :: KExpression -> Instruction
-koakToLLVM expr = ((binOpConvert . getBinOp) expr) AST.noFastMathFlags ((intConvert . getFirstInt) expr) ((intConvert . getSecondInt) expr) []
+kExpressionToLInstruction :: KExpression -> Instruction
+kExpressionToLInstruction expr =
+    ((binOpConvert . getBinOp) expr)
+        AST.noFastMathFlags
+        ((kLiteralToLOperand . getFirstKLiteral) expr)
+        ((kLiteralToLOperand . getSecondKLiteral) expr)
+        []
 
 getBinOp :: KExpression -> KBinOp
 getBinOp (KExpression _ [(binOp, _)]) = binOp
 
-getFirstInt :: KExpression -> KLiteral
-getFirstInt (KExpression (KPostfix (KPrimary primary)) _) = getValueFromPrimary primary
+getFirstKLiteral :: KExpression -> KLiteral
+getFirstKLiteral (KExpression (KPostfix (KPrimary primary)) _) = getValueFromPrimary primary
 
-getSecondInt :: KExpression -> KLiteral
-getSecondInt (KExpression _ [(_, (KPostfix (KPrimary primary)))]) = getValueFromPrimary primary
+getSecondKLiteral :: KExpression -> KLiteral
+getSecondKLiteral (KExpression _ [(_, (KPostfix (KPrimary primary)))]) = getValueFromPrimary primary
 
 getValueFromPrimary :: KPrimary -> KLiteral
 getValueFromPrimary (KLiteral x) = x
 
-intConvert :: KLiteral -> Operand
-intConvert (KDecimalConst x) = ConstantOperand (C.Int 32 (toInteger x))
+kLiteralToLOperand :: KLiteral -> Operand
+kLiteralToLOperand (KDecimalConst x) = ConstantOperand (C.Int 32 (toInteger x))
+kLiteralToLOperand (KDoubleConst x) = ConstantOperand (C.Float (F.Single (realToFrac x)))
 
 binOpConvert :: KBinOp -> FastMathFlags -> Operand -> Operand -> InstructionMetadata -> Instruction
 binOpConvert KBinOpLess = AST.FSub
-
--- src/LLVMTest.hs
+++ src/LLVMTest.hs
@@ -2,46 +2,31 @@
 
 module LLVMTest where
 
-import LLVM.AST as AST
-import LLVM.AST.CallingConvention as AST
-import LLVM.AST.Constant as C
-import LLVM.AST.Global
-import LLVM.AST.IntegerPredicate as AST
-import LLVM.AST.Type as AST
-import LLVM.AST.AddrSpace as AST
+import           LLVM.AST                   as AST
+import           LLVM.AST.AddrSpace         as AST
+import           LLVM.AST.CallingConvention as AST
+import           LLVM.AST.Constant          as C
+import           LLVM.AST.Float             as F
+import           LLVM.AST.Global
+import           LLVM.AST.IntegerPredicate  as AST
+import           LLVM.AST.Type              as AST
 
-import LLVM.Context
-import LLVM.Module
-import LLVM.Target
-
+import           LLVM.Context
+import           LLVM.Module
+import           LLVM.Target
 
 defMin :: Definition
-defMin = GlobalDefinition functionDefaults
-    { name = Name "min"
-    , parameters =
-        ( [ Parameter AST.i32 (Name "a") []
-          , Parameter AST.i32 (Name "b") [] ]
-        , False
-        )
-    , returnType = AST.i32
-    , basicBlocks = [entry]
-    }
-    where
-        entry = BasicBlock
+defMin =
+    GlobalDefinition
+        functionDefaults {name = Name "sub", parameters = ([], False), returnType = AST.i32, basicBlocks = [entry]}
+  where
+    entry =
+        BasicBlock
             (Name "entry")
-            [Name "res" := AST.FSub
-                AST.noFastMathFlags
-                (LocalReference AST.i32 (Name "a"))
-                (LocalReference AST.i32 (Name "b"))
-                []
+            [ Name "res" :=
+              AST.FSub AST.noFastMathFlags (ConstantOperand (C.Int 32 1)) (ConstantOperand (C.Int 32 1)) []
             ]
-            (Do $ Ret
-                (Just $ LocalReference AST.i1 (Name "res"))
-                []
-            )
+            (Do $ Ret (Just $ LocalReference AST.i32 (Name "res")) [])
 
 llvmTestModule :: AST.Module
-llvmTestModule = defaultModule
-    { moduleName = "min"
-    , moduleDefinitions = [defMin]
-    }
+llvmTestModule = defaultModule {moduleName = "min", moduleDefinitions = [defMin]}
