7b469b33bceaa8eed0f3b8a68f936276a08811d7
arthur <arthur.teisseire@epitech.eu>
2020-02-19 10:10:17 +0100

    Link pipeline from file to llvm output

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,52 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-import LLVM.AST as AST
-import LLVM.AST.CallingConvention as AST
-import LLVM.AST.Constant as C
-import LLVM.AST.Global
-import LLVM.AST.IntegerPredicate as AST
-import LLVM.AST.Type as AST
-import LLVM.AST.AddrSpace as AST
+import           LLVM.AST                   as AST
+import           LLVM.AST.AddrSpace         as AST
+import           LLVM.AST.CallingConvention as AST
+import           LLVM.AST.Constant          as C
+import           LLVM.AST.Global
+import           LLVM.AST.IntegerPredicate  as AST
+import           LLVM.AST.Type              as AST
 
-import LLVM.Context
-import LLVM.Module
-import LLVM.Target
+import           LLVM.Context
+import           LLVM.Module
+import           LLVM.Target
 
-import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Char8      as BS
 
-import LLVMTestOp
-import LLVMTestMain
+import           LLVMTestMain
+import           LLVMTestOp
+
+import           System.Environment
+import           System.Exit
+import           System.IO
+
+import           KoakToLLVM
+import           Parser
 
 main :: IO ()
-main = withContext $ \ctx -> do
---    tm <- getDefaultTargetTriple
---    writeObjectToFile tm (File "a.out") llvmTestModule
-    withModuleFromAST ctx llvmTestMain (writeBitcodeToFile $ File "test.ll")
-    withModuleFromAST ctx llvmTestOp (writeBitcodeToFile $ File "op.ll")
+main = getArgs >>= interpretFile . head >>= printLLVM . llvmTestModule
+
+interpretFile :: String -> IO Definition
+interpretFile filename = openFile filename ReadMode >>= hGetContents >>= srcToDef
+
+srcToDef :: String -> Definition
+srcToDef src = kDefToGlobalDef <$> parse src
+  where
+    parse src =
+        case parseKoak src of
+            Left errorMsg -> putStrLn errorMsg >> exitWith (ExitFailure 84)
+            Right koakAst -> pure koakAst
+
+llvmTestModule :: Definition -> AST.Module
+llvmTestModule def = defaultModule {moduleName = "main", moduleDefinitions = [def]}
+
+printLLVM :: AST.Module -> IO ()
+printLLVM m =
+    withContext $ \ctx -> do
+        llvm <- withModuleFromAST ctx m moduleLLVMAssembly
+        BS.putStrLn llvm
+--            withModuleFromAST ctx llvmTestMain (writeBitcodeToFile $ File "test.ll")
+--            withModuleFromAST ctx llvmTestOp (writeBitcodeToFile $ File "op.ll")
