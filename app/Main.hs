9df18420c72ba5b2b8d5d700fe28593b6f708757
arthur <arthur.teisseire@epitech.eu>
2020-02-23 12:49:37 +0100

    Refactor main

--- app/Main.hs
+++ app/Main.hs
@@ -20,10 +20,7 @@ main :: IO ()
 main = getArgs >>= mapM interpretFile >>= mapM_ (printLLVM . llvmTestModule)
 
 interpretFile :: String -> IO [Definition]
-interpretFile filename = openFile filename ReadMode >>= hGetContents >>= srcToDefM
-
-srcToDefM :: String -> IO [Definition]
-srcToDefM s = either dieWithMsg pure (srcToDef s)
+interpretFile filename = openFile filename ReadMode >>= hGetContents >>= eitherToIO . srcToDef
 
 srcToDef :: String -> Either String [Definition]
 srcToDef s = koakToLLVM <$> parseKoak s
@@ -38,6 +35,9 @@ printLLVM m =
         BS.putStrLn llvm
 --            withModuleFromAST ctx llvmTestMain (writeBitcodeToFile $ File "test.ll")
 --            withModuleFromAST ctx llvmTestOp (writeBitcodeToFile $ File "op.ll")
+
+eitherToIO :: Either String a -> IO a
+eitherToIO = either dieWithMsg pure
 
 dieWithMsg :: String -> IO a
 dieWithMsg s = putStrLn s >> exitWith (ExitFailure 84)
