243f13e1a8abd6e8dfc73c1d1ba43e5321c344a2
arthur <arthur.teisseire@epitech.eu>
2020-02-26 09:38:48 +0100

    Refactor main

--- app/Main.hs
+++ app/Main.hs
@@ -11,30 +11,30 @@ import qualified Data.ByteString.Char8 as BS
 
 import           System.Environment
 import           System.Exit
-import           System.IO
 
-import           KoakToLLVM
-import           Parser
+import           Control.Monad ((>=>))
+import           KoakToLLVM (koakToLLVM)
+import           Parser (parseKoak)
 
 main :: IO ()
-main = getArgs >>= mapM interpretFile >>= mapM_ (printLLVM . llvmTestModule)
+main = getArgs >>= mapM_ printLLVMFromFilename
+
+printLLVMFromFilename :: String -> IO ()
+printLLVMFromFilename s = interpretFile s >>= llvmModuleToString . mkModule >>= BS.putStrLn
 
 interpretFile :: String -> IO [Definition]
-interpretFile filename = openFile filename ReadMode >>= hGetContents >>= eitherToIO . srcToDef
+interpretFile = readFile >=> srcToDef
 
-srcToDef :: String -> Either String [Definition]
-srcToDef s = koakToLLVM <$> parseKoak s
+srcToDef :: String -> IO [Definition]
+srcToDef s = koakToLLVM <$> parse s
+  where
+    parse = eitherToIO . parseKoak
 
-llvmTestModule :: [Definition] -> AST.Module
-llvmTestModule def = defaultModule {moduleName = "main", moduleDefinitions = def}
+mkModule :: [Definition] -> AST.Module
+mkModule def = defaultModule {moduleName = "main", moduleDefinitions = def}
 
-printLLVM :: AST.Module -> IO ()
-printLLVM m =
-    withContext $ \ctx -> do
-        llvm <- withModuleFromAST ctx m moduleLLVMAssembly
-        BS.putStrLn llvm
---            withModuleFromAST ctx llvmTestMain (writeBitcodeToFile $ File "test.ll")
---            withModuleFromAST ctx llvmTestOp (writeBitcodeToFile $ File "op.ll")
+llvmModuleToString :: AST.Module -> IO BS.ByteString
+llvmModuleToString m = withContext $ \ctx -> withModuleFromAST ctx m moduleLLVMAssembly
 
 eitherToIO :: Either String a -> IO a
 eitherToIO = either dieWithMsg pure
